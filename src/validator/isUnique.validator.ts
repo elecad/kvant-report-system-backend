3f82cac28f987a7820935c48a904c681f14ca511
Anton Dachin <relas1234@gmail.com>
2022-06-22 11:51:35 +0300

    Fixed bug in isUnique validator

--- src/validator/isUnique.validator.ts
+++ src/validator/isUnique.validator.ts
@@ -9,7 +9,11 @@ export class isUnique implements ValidatorConstraintInterface {
   async validate(text: string | number, args: ValidationArguments) {
     const { model, where }: { model; where?: string } = args.constraints[0];
 
-    if (!text || typeof text != 'string') return false;
+    const types = ['string', 'number'];
+
+    if (!text || !types.find((t) => t === typeof text) || isNaN(+text))
+      return false;
+
     if (where)
       return !!!(await model.findOne({
         where: { [where]: text },
