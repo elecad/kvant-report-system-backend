03130405f6a95d016721798e2dd7d7c94400bf26
Anton Dachin <relas1234@gmail.com>
2022-07-28 18:45:15 +0300

    Added School Controller

--- src/modules/school/school.controller.ts
+++ src/modules/school/school.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
+import { parseIntOptions } from 'src/validators/options/parseIntPipe.option';
 
 @Controller('school')
 export class SchoolController {
@@ -18,17 +28,25 @@ export class SchoolController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.schoolService.findOne(+id);
+  findOne(@Param('id', new ParseIntPipe(parseIntOptions)) id: number) {
+    return this.schoolService.validateOne({
+      type: 'existing',
+      column: 'id',
+      value: id,
+      // findOptions: { include: DependencyType },
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
-    return this.schoolService.update(+id, updateSchoolDto);
+  update(
+    @Param('id', new ParseIntPipe(parseIntOptions)) id: number,
+    @Body() updateSchoolDto: UpdateSchoolDto,
+  ) {
+    return this.schoolService.update(id, updateSchoolDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', new ParseIntPipe(parseIntOptions)) id: number) {
     return this.schoolService.remove(+id);
   }
 }
