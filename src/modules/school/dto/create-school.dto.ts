4445e7077127fcca2dc88f121885a3940cc46714
Anton Dachin <relas1234@gmail.com>
2022-07-28 18:14:31 +0300

    Adeed create-dto for School entity

--- src/modules/school/dto/create-school.dto.ts
+++ src/modules/school/dto/create-school.dto.ts
@@ -1,1 +1,36 @@
-export class CreateSchoolDto {}
+import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { STRINGS } from 'src/res/strings';
+
+export class CreateSchoolDto {
+  @IsNotEmpty({
+    message: STRINGS.IsNotEmptyError,
+  })
+  @IsString({
+    message: STRINGS.IsStringError,
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: STRINGS.IsNotEmptyError,
+  })
+  @IsString({
+    message: STRINGS.IsStringError,
+  })
+  adress: string;
+
+  @IsNotEmpty({
+    message: STRINGS.IsNotEmptyError,
+  })
+  @IsPositive({
+    message: STRINGS.IsPositiveError,
+  })
+  dependency_id: number;
+
+  @IsNotEmpty({
+    message: STRINGS.IsNotEmptyError,
+  })
+  @IsPositive({
+    message: STRINGS.IsPositiveError,
+  })
+  school_type_id: number;
+}
