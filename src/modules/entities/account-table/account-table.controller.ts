02363a5db7823af3cbce6b091b0386385ba0d374
Anton Dachin <relas1234@gmail.com>
2022-08-14 15:45:01 +0300

    Added CRUD account entity

--- src/modules/entities/account-table/account-table.controller.ts
+++ src/modules/entities/account-table/account-table.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { parseIntOptions } from 'src/validators/options/parseIntPipe.option';
 import { AccountTableService } from './account-table.service';
 import { CreateAccountTableDto } from './dto/create-account-table.dto';
 import { UpdateAccountTableDto } from './dto/update-account-table.dto';
@@ -18,17 +28,24 @@ export class AccountTableController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.accountTableService.findOne(+id);
+  findOne(@Param('id', new ParseIntPipe(parseIntOptions)) id: number) {
+    return this.accountTableService.validateOne({
+      column: 'id',
+      type: 'existing',
+      value: id,
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAccountTableDto: UpdateAccountTableDto) {
-    return this.accountTableService.update(+id, updateAccountTableDto);
+  update(
+    @Param('id', new ParseIntPipe(parseIntOptions)) id: number,
+    @Body() updateAccountTableDto: UpdateAccountTableDto,
+  ) {
+    return this.accountTableService.update(id, updateAccountTableDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.accountTableService.remove(+id);
+  remove(@Param('id', new ParseIntPipe(parseIntOptions)) id: number) {
+    return this.accountTableService.remove(id);
   }
 }
--- src/modules/entities/account-table/account-table.service.ts
+++ src/modules/entities/account-table/account-table.service.ts
@@ -1,26 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { FindOptions } from 'sequelize';
+import {
+  databaseValidateAll,
+  databaseValidateOne,
+  ValidateOption,
+} from 'src/validators/database.validator';
 import { CreateAccountTableDto } from './dto/create-account-table.dto';
 import { UpdateAccountTableDto } from './dto/update-account-table.dto';
+import { AccountTable } from './entities/account-table.entity';
 
 @Injectable()
 export class AccountTableService {
-  create(createAccountTableDto: CreateAccountTableDto) {
-    return 'This action adds a new accountTable';
+  constructor(
+    @InjectModel(AccountTable) private repository: typeof AccountTable,
+  ) {}
+
+  entityName = 'Аккаунт';
+
+  async create(createAccountTableDto: CreateAccountTableDto) {
+    const { email } = createAccountTableDto;
+    await this.validateOne({ column: 'email', type: 'unique', value: email });
+    return this.repository.create(createAccountTableDto);
   }
 
-  findAll() {
-    return `This action returns all accountTable`;
+  findAll(option: FindOptions<AccountTable> = {}) {
+    return this.repository.findAll(option);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} accountTable`;
+  findOne(option: FindOptions<AccountTable> = {}) {
+    return this.repository.findOne(option);
   }
 
-  update(id: number, updateAccountTableDto: UpdateAccountTableDto) {
-    return `This action updates a #${id} accountTable`;
+  async update(id: number, updateAccountTableDto: UpdateAccountTableDto) {
+    const { email } = updateAccountTableDto;
+
+    const entity = await this.validateOne({
+      type: 'existing',
+      column: 'id',
+      value: id,
+    });
+
+    if (entity.email !== email)
+      await this.validateOne({
+        type: 'unique',
+        column: 'email',
+        value: email,
+      });
+
+    return entity.update(updateAccountTableDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} accountTable`;
+  async remove(id: number) {
+    const entity = await this.validateOne({
+      type: 'existing',
+      column: 'id',
+      value: id,
+    });
+
+    await entity.destroy();
+  }
+
+  async validateOne(props: ValidateOption<AccountTable>) {
+    //? Одиночный валидатор
+    return databaseValidateOne(AccountTable, this.entityName, props);
+  }
+
+  async validateAll(props: ValidateOption<AccountTable>[]) {
+    //? Групповой валидатор
+    return databaseValidateAll(AccountTable, this.entityName, props);
   }
 }
